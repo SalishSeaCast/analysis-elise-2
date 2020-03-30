f0b6ac7fb40a2e042f7b730b9899075a001aaaa4
Elise Olson <eolson@eoas.ubc.ca>
2020-03-29 23:25:37 -0700

    new version of HC1905 eval using papermill

---
+++ notebooks/bioTuning/HC1905-varvar/repro-vET-HC1905-DFO-NutChlPhys.py
@@ -0,0 +1,10 @@
+import papermill as pm
+
+paramlist=list()
+for yr in range(2007,2019):
+    pm.execute_notebook(
+       'vET-HC1905-DFO-NutChlPhys-Base.ipynb',
+       'vET-HC1905-DFO-NutChlPhys-'+str(yr)+'.ipynb',
+       parameters=dict(year=yr,)
+        );
+
