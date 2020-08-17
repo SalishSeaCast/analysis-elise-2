c926379620993241b330cb1e9c05318c9f229d5f
e-olson <eolson@eoas.ubc.ca>
2020-08-16 20:16:51 -0700

    mostly work on HPLC

--- notebooks/ncoScripts/proc_fluxesCorr_2.py
+++ notebooks/ncoScripts/proc_fluxesCorr_2.py
@@ -61,12 +61,47 @@ def copy_ptrc_T(fnames,fnum):
         print(pids[ipid].returncode)
     return pids
 
+def copy_gen(fnames,fnum,keepvars):
+    pids=dict()
+    for ii in range(0,fnum):
+        f0=fnames[ii]
+        fpl=os.path.join(saveloc,f0[len(spath):])
+        pids[ii]=subprocess.Popen('nccopy -d4 -V '+','.join(keepvars)+' '+f0+' '+fpl, shell=True,
+                                       stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
+        if ii%maxproc==(maxproc-1):
+            pids[ii].wait() # wait for the last one in set
+    pids[ii].wait() # wait for the last one
+    # check that no others are still running, wait for them
+    for ipid in pids.keys():
+        while pids[ipid].poll() is None:
+            time.sleep(30)
+    for ipid in pids.keys():
+        for line in pids[ipid].stdout:
+            print(line)
+        print(pids[ipid].returncode)
+    return pids
+
 if __name__ == "__main__":
     #ftype='snp_T'
     #fnames,fnum=setup(ftype)
     #pids = copy_snp_T(fnames,fnum);
     #print('done')
-    ftype='ptrc_T'
+    #ftype='ptrc_T'
+    #fnames,fnum=setup(ftype)
+    #pids = copy_ptrc_T(fnames,fnum);
+    #print('done')
+    ftype='dia1_T'
+    keepvars=('time_centered', 'ATF_NO3', 'ATF_NH4', 'ATF_DON', 'ATF_PON', 'ATF_LIV', 'BFX_PON', 'BFX_DIAT')
     fnames,fnum=setup(ftype)
-    pids = copy_ptrc_T(fnames,fnum);
-    print('done')
+    pids = copy_gen(fnames,fnum,keepvars);
+    print('done dia1_T')
+    ftype='dia2_T'
+    keepvars=('time_centered', 'SMS_NO3', 'SMS_NH4', 'SMS_DON', 'SMS_PON', 'SMS_LIV')
+    fnames,fnum=setup(ftype)
+    pids = copy_gen(fnames,fnum,keepvars);
+    print('done dia2_T')
+    ftype='dian_T'
+    keepvars=('time_centered', 'RIV_NO3', 'RIV_NH4', 'REM_NO3', 'REM_PON', 'REM_DON', 'PRD_NO3', 'PRD_NH4')
+    fnames,fnum=setup(ftype)
+    pids = copy_gen(fnames,fnum,keepvars);
+    print('done dian_T')
