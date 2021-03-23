7890dd99901faebfa1de367e392d13fef0e5116d
e-olson <eolson@eoas.ubc.ca>
2021-03-23 13:35:40 -0700

    reorganize

--- startNBserver/jupPAR.sh
+++ startNBserver/jupPAR.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /ocean/eolson/MEOPAR/TPARCalcs/notebooks
+cd /ocean/eolson/MEOPAR/TPARCalcs
 tmux new -d -s jupPAR
 tmux send-keys -t jupPAR.0 "jupyter lab --no-browser --port=8583" ENTER
