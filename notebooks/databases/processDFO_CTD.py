16da9272c08435c33245cae5dcacd028f0c64c51
e-olson <eolson@eoas.ubc.ca>
2021-06-22 22:26:07 -0700

    remake DFO_CTD database with 2013 data and rerun TS clusters for TJ

--- notebooks/databases/processDFO_CTD.py
+++ notebooks/databases/processDFO_CTD.py
@@ -132,13 +132,13 @@ def main():
         session0.query(ObsTBL0).filter(ObsTBL0.StationTBLID==bID).update({"Include":False})
         session0.commit()
         
-        # In case files are re-extracted from the zip obtained from DFO, check 
-        # file EO UBC November 16, 2018 (2015 data)/2015-29-0002.ctd date
-        # and correct if needed. Should be August 2, 2015, not February 8, 2015. 
-        fnameErr='req20181116/EO UBC November 16, 2018 (2015 data)/2015-29-0002.ctd'
-        if session0.query(StationTBL0).filter(and_(StationTBL0.sourceFile==fnameErr,StationTBL0.StartMonth==2)).count():
-            session0.query(StationTBL0).filter(StationTBL0.sourceFile==fnameErr).update({"StartMonth":8,"StartDay":2})
-            session0.commit()
+    # In case files are re-extracted from the zip obtained from DFO, check 
+    # file EO UBC November 16, 2018 (2015 data)/2015-29-0002.ctd date
+    # and correct if needed. Should be August 2, 2015, not February 8, 2015. 
+    fnameErr='req20181116/EO UBC November 16, 2018 (2015 data)/2015-29-0002.ctd'
+    if session0.query(StationTBL0).filter(and_(StationTBL0.sourceFile==fnameErr,StationTBL0.StartMonth==2)).count():
+        session0.query(StationTBL0).filter(StationTBL0.sourceFile==fnameErr).update({"StartMonth":8,"StartDay":2})
+        session0.commit()
 
     session0.close()
     engine0.dispose()
